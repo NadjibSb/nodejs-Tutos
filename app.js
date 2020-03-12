dc036d0511958e8da976b1e87508f42be8ae0a8e
NadjibSb <fn_souab@esi.dz>
2020-03-12 18:13:52 +0100

    Todo CRD functions

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var todoCtrl = require("./controllers/todoController");
+
+var app = express();
+
+app.set('view engine','ejs');
+app.use(express.static("./public"));
+todoCtrl(app);
+
+app.listen(3000);
