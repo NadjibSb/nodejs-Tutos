5fecc0746297f8e44870575350a74684949223fd
NadjibSb <fn_souab@esi.dz>
2020-03-12 22:00:01 +0100

    integrating with MongoDb

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 
 app.set('view engine','ejs');
 app.use(express.static("./public"));
+
 todoCtrl(app);
 
 app.listen(3000);
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,27 +1,31 @@
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-var urlEncodedParser = bodyParser.urlencoded({extended: false});
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
-var data = [{item: "making bed"},{item: "making breakfest"},{item: "studying"}]
+mongoose.connect('mongodb://localhost:27017/TodoDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const Todo = mongoose.model('Todo', { item: String });
 
 module.exports = function(app){
 
-    console.log("starts");
-
     app.get("/todo", function(req,res){
-        res.render('todo',{todos: data});
+        Todo.find({}, function(err,data){
+            if(err) throw err;
+            res.render('todo',{todos: data});
+        });
     });
 
     app.post('/todo', urlEncodedParser, function(req,res){
-        data.push(req.body);
-        res.json(data);
+        var newItem = Todo(req.body).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
     
     app.delete("/todo/:item", function(req,res){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item; 
-        })
-        res.json(data);
+        Todo.find({item: req.params.item.replace(/-/g,' ')}).remove(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        });
     });
-
 };
